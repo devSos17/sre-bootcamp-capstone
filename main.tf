308b5c23119853cb43e5a1b8138225e1fee4cb43
Santiago Orozco <devsos117@gmail.com>
2023-03-29 18:36:37 -0600

    Update no trivy

--- main.tf
+++ main.tf
@@ -1,28 +1,26 @@
 terraform {
   backend "s3" {}
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 }
+
+
 
 provider "aws" {
-  version = "~> 3.0,!= 3.14.0"
-  region  = "us-east-2" # Update with your preferred region
+  region = us-east-1
 }
 
-resource "aws_instance" "state_ws6_daniela_ec2" {
-  ami           = "ami-074cce78125f09d61" # For us-east-2
-  instance_type = "t2.micro"
-  user_data     = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y docker
-              EOF
-  tags = {
-    Name        = "state-ws6-daniela-ec2" # Update the name
-    Terraform   = "true"
-    Service     = "EC2"
-    Project     = "sre-bootcamp-2023-ws6"
-    Environment = terraform.workspace
-    Count       = "${terraform.workspace == "default" ? 5 : 1}"
-  }
+module "ec2" {
+  source             = "./modules/ec2"
+  security_group_ids = [module.sg.sg_id]
+  region             = var.region
+  owner              = var.owner
+  message            = "Workshop Demo"
 }
 
 # resource "aws_security_group" "public_sg" {
